73d08503e5fdeed4fb1e4967218db219006a8ca0
Elaine Matos <enimatos@live.com>
2024-11-17 16:53:16 -0300

    Primeiro commit do curso de IA da EdN

---
+++ exercicios/aula02/operadores_aritmeticos.py
@@ -0,0 +1,10 @@
+
+a, b = 10, 3
+
+print(f"Adição: {a + b}")
+print(f"Subtração: {a + b}")
+print(f"Multiplicacao: {a + b}")
+print(f"Divisao: {a + b}")
+print(f"Adição: {a + b}")
+print(f"Adição: {a + b}")
+print(f"Adição: {a + b}")
