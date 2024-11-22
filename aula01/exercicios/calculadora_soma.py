a6b15546626488d466da935456e335dbfae7e5a5
Elaine Matos <69444237+enimatos@users.noreply.github.com>
2024-11-22 00:30:10 -0300

    Delete aula01/exercicios directory

--- aula01/exercicios/calculadora_soma.py
+++
@@ -1,8 +0,0 @@
-# Calculadora de dois números
-
-numero1 = float(input("Digite o priomeiro número: "))
-
-numero2 = float(input("Digite o segundo número: "))
-
-soma= numero1 + numero2
-print(f"A soma do {numero1} e do {numero2} é {soma} .")
