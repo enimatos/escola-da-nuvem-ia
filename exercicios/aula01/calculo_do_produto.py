8dee5f800c877f0b5b860ec5063f17a815360a79
Elaine Matos <69444237+enimatos@users.noreply.github.com>
2024-11-22 00:29:01 -0300

    Delete exercicios directory

--- exercicios/aula01/calculo_do_produto.py
+++
@@ -1,8 +0,0 @@
-# Calculo do Produto
-
-numero1 = float(input("Coloque o primeiro número: "))
-numero2 = float(input("Coloque o segundo número: "))
-
-produto = numero1 * numero2
-
-print (f"O produto entre {numero1} e {numero2} é {int(produto)}")
